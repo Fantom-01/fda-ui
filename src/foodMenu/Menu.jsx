66dff8f2f271c607873edc97f5ddfe10c466e75a
Fantom <93797224+Fantom-01@users.noreply.github.com>
2023-02-15 16:16:16 +0100

    foodmenu styling complete

--- src/foodMenu/Menu.jsx
+++ src/foodMenu/Menu.jsx
@@ -6,14 +6,26 @@ const Menu = ({ items }) => {
       {items.map((menuItem) => {
         const { id, title, img, desc, price } = menuItem;
         return (
+          // <article key={id} className="menu-item">
+          //   <img src={img} alt={title} className="photo" />
+          //   <div className="item-info">
+          //     <header>
+          //       <h4>{title}</h4>
+          //       <h4 className="price">${price}</h4>
+          //     </header>
+          //     <p className="item-text">{desc}</p>
+          //   </div>
+          // </article>
           <article key={id} className="menu-item">
-            <img src={img} alt={title} className="photo" />
-            <div className="item-info">
-              <header>
-                <h4>{title}</h4>
-                <h4 className="price">${price}</h4>
-              </header>
-              <p className="item-text">{desc}</p>
+            <div className="product--image">
+              <img src={img} alt={title} className="photo" />
+            </div>
+            <div className="product--text">
+              <h4><a href="#">{title}</a></h4>
+              <div className="price--div">
+                <span><h4 className="price">${price}</h4></span>
+                <button className="to--cart--btn">Add To Cart</button>
+              </div>
             </div>
           </article>
         );
