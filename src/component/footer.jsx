87d2125ff0ef9a1787dbce13ca510ff122cfba58
Fantom <93797224+Fantom-01@users.noreply.github.com>
2023-02-21 17:34:37 +0100

    testing view...

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -6,27 +6,27 @@ const footer = () => {
   return (
     <footer>
         <section className='footer--one'>
-            <div>
+            <div className='footer--one--div'>
                 <div className="footer--logo--div">
                     <img src={logo} alt="" width='71px' height="71px"/>
                     <caption className="footer--logo--caption" >Tasty Treat</caption>
                 </div>
                 <p className='footer--caption--p'>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Nesciunt pariatur <br /> accusamus</p>
             </div>
-            <div>
+            <div className='footer--one--div'>
                 <h3>Delivery Time</h3>
                 <h5 className='footer--h5'>Sunday - Thursday</h5>
                 <p className='footer--one--div--p'>10:00am to 11:00pm</p>
                 <h5 className='footer--h5'>Friday - Saturday</h5>
                 <p className='footer--one--div--p'>Off day</p>
             </div>
-            <div>
+            <div className='footer--one--div'>
                 <h3>Contact</h3>
                 <p className='footer--one--div--p'>Location: ZindaBazar, Sylhet-3100, Bangladesh</p>
                 <h5 className='footer--h5'>Phone: 01712345678</h5>
                 <h5 className='footer--h5'>Email: example@gmail.com</h5>
             </div>
-            <div>
+            <div className='footer--one--div'>
                 <h3>Newsletter</h3>
                 <p className='footer--one--div--p'>Subscribe to our Newsletter</p>
                 <div className="newsletter">
