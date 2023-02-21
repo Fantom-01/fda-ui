f5b999a43d07a66a4f37bb6a424596b94925a89f
Fantom <93797224+Fantom-01@users.noreply.github.com>
2023-02-21 07:35:25 +0100

    testing view

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -7,9 +7,25 @@ import { useState } from "react";
 
 const navbar = () => {
   const [isOpen, setOpen] = useState(false);
+
+
   return (
     <>
-      <nav>
+      {/* <div id="menu">
+        <div id="menu-icon-div">
+          <img src="/assets/images/icon-menu-close.svg" alt="" id="menu-icon-close">
+        </div>
+
+        <div id="menu-list">
+          <a href="#" class="menu--links">Home</a>
+          <a href="#" class="menu--links">New</a>
+          <a href="#" class="menu--links">Popular</a>
+          <a href="#" class="menu--links">Trending</a>
+          <a href="#" class="menu--links">Categories</a>
+        </div>
+      </div> */}
+  
+      <nav className="nav">
         <div className="nav--logo--div">
           <img src={logo} alt="" width='71px' height="71px"/>
           <caption className="logo--caption">Tasty Treat</caption>
@@ -25,6 +41,7 @@ const navbar = () => {
         <div className="nav--icon">
           <a href="#" className="nav--a"><i class="fas fa-basket-shopping"></i></a>
           <a href="#" className="nav--a"><i class="fas fa-user"></i></a>
+          <a id="menu-icon" className="nav--a"><i class="fas fa-bars"></i></a>
         </div>
       </nav>
     </>
