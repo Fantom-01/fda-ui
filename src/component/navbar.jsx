d4eba875e1d316b9d76bc90c0524bfb25ae6057e
Fantom <93797224+Fantom-01@users.noreply.github.com>
2023-02-22 07:11:17 +0100

    Testing responsiveness

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import logo from "../assets/res-logo.png";
 
-
 import { useState } from "react";
+import { useEffect } from "react";
+
+// START SHIT HERE!!!!!!!!!!!!!!!!!!!!!
+
+const Menu = () => {
+  
+  return (
+    <div id="sidemenu">
+      <section className="transparent--half"></section>
+      <section className="main--half">
+          <div id="menu-icon-div">
+            {/* <a id="menu-icon" className="nav--a" onClick={() => setShow(false)}><i class="fas fa-bars"></i></a> */}
+          </div>
+
+          <div id="sidemenu-list">
+            <a href="#" class="sidemenu--links">Home</a>
+            <a href="#" class="sidemenu--links">Food</a>
+            <a href="#" class="sidemenu--links">Cart</a>
+            <a href="#" class="sidemenu--links">Contact</a>
+          </div>
+      </section>
+    </div>
+  )
+}
+// END SHIT HERE!!!!!!!!!!!!!!!!!!!!!
 
 
 const navbar = () => {
-  const [isOpen, setOpen] = useState(false);
-
+  const [show, setShow] = useState(false);
 
   return (
     <>
-      {/* <div id="menu">
-        <div id="menu-icon-div">
-          <img src="/assets/images/icon-menu-close.svg" alt="" id="menu-icon-close">
-        </div>
-
-        <div id="menu-list">
-          <a href="#" class="menu--links">Home</a>
-          <a href="#" class="menu--links">New</a>
-          <a href="#" class="menu--links">Popular</a>
-          <a href="#" class="menu--links">Trending</a>
-          <a href="#" class="menu--links">Categories</a>
-        </div>
-      </div> */}
+    {show && <Menu/>}
   
       <nav className="nav">
         <div className="nav--logo--div">
@@ -31,8 +42,6 @@ const navbar = () => {
           <caption className="logo--caption">Tasty Treat</caption>
         </div>
         <div className="nav--links">
-
-          
           <a className="nav--a" exact href="/">Home</a>
           <a className="nav--a" href="/food">Food</a>
           <a className="nav--a" href="/cart">Cart</a>
@@ -41,7 +50,7 @@ const navbar = () => {
         <div className="nav--icon">
           <a href="#" className="nav--a"><i class="fas fa-basket-shopping"></i></a>
           <a href="#" className="nav--a"><i class="fas fa-user"></i></a>
-          <a id="menu-icon" className="nav--a"><i class="fas fa-bars"></i></a>
+        <a id="menu-icon" className="nav--a" onClick={() => setShow(!show)}><i class="fas fa-bars"></i></a>
         </div>
       </nav>
     </>
